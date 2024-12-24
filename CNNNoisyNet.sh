78e267ea8dac00d278a3ee1105265e962e270682
zhanghanxing <20302010045@fudan.edu.cn>
2024-12-24 15:30:12 +0800

    abc

--- CNNNoisyNet.sh
+++ CNNNoisyNet.sh
@@ -1,9 +1,9 @@
 python "Snake-DQN_lrDouble DQNandCNNNoisyNet.py" \
     --gridsize 15 \
-    --num_episodes 1200 \
+    --num_episodes 3000 \
     --target_update_frequency 5 \
-    --lr 5e-5 \
+    --lr 1e-3 \
     --num_updates 20 \
     --batch_size 512 \
     --num_games 30 \
-    --checkpoint_dir ./CNNnoisynet_Tr5e-5_tgt5_iter20 
+    --checkpoint_dir ./CNNnoisynet_Tr1e-3_tgt5_iter20_3000
